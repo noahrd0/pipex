cc20c9e9f709e46d03dd1358255e9ef3a196c658
nradal <no.radal1203@gmail.com>
2022-09-30 01:42:17 +0200

    srcs/pipex.c with memory leaks

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   pipi.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nradal <nradal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/22 11:25:04 by noahrd0           #+#    #+#             */
-/*   Updated: 2022/09/29 16:20:05 by nradal           ###   ########.fr       */
+/*   Created: 2022/09/30 01:18:45 by nradal            #+#    #+#             */
+/*   Updated: 2022/09/30 01:38:16 by nradal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,7 +103,6 @@ void leave_clean(char **cmds, char *path)
 	i = 0;
 	while (cmds[i])
 	{
-		printf("%d : %s\n", i, cmds[i]);
 		free(cmds[i]);
 		i++;
 	}
@@ -128,7 +127,9 @@ int	infile_process(char **av, int *pipe_fd, char **envp, char *path, char **cmds
 					if (close(infile_fd) != -1)
 						if (close(pipe_fd[1]) != -1)
 							return (execve(path, cmds, envp));
+		leave_clean(cmds, path);
 		exit_error("error in infile_process", 2);
+		return(-1);
 	}
 	if (waitpid(pid, NULL, 0) == -1)
 	{
@@ -156,7 +157,8 @@ int	outfile_process(char **av, int *pipe_fd, char **envp, char *path, char **cmd
 					if (close(outfile_fd) != -1)
 						if (close(pipe_fd[0]) != -1)
 							return (execve(path, cmds, envp));
-	exit_error("error in outfile_process", 2);
+		leave_clean(cmds, path);
+		return(-1);
 	}
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
@@ -168,6 +170,16 @@ int	outfile_process(char **av, int *pipe_fd, char **envp, char *path, char **cmd
 	leave_clean(cmds, path);
 	return (EXIT_SUCCESS);
 }
+
+	// close(pipe_fd[0]);
+	// close(pipe_fd[1]);
+	// if (waitpid(pid, NULL, 0) == -1)
+	// {
+	// 	leave_clean(cmds, path);
+	// 	exit_error("waitpid in infile", 2);
+	// }
+	// leave_clean(cmds, path);
+	// return (EXIT_SUCCESS);
 
 int	main(int ac, char **av, char **envp)
 {
@@ -190,72 +202,9 @@ int	main(int ac, char **av, char **envp)
 			exit_error("command not found", 2);
 		cmds = ft_split(av[3], ' ');
 		if (outfile_process(av, pipe_fd, envp, path, cmds) == -1)
-			exit_error("execve in infile_process", 2);
+			exit_error("execve in outfile_process", 2);
 	}
 	else
 		exit_error("number of arguments", 2);
 	return (0);
 }
-
-
-/*int	execution(char **av, int *pipe_fd, pid_t pid, char **envp)
-{
-	int		infile_fd;
-	int		outfile_fd;
-	
-	if (pid == 0)	// CHILD PROCESS, WRITE
-	{
-		infile_fd = open_file(av[1], INFILE);
-		if (close(pipe_fd[0]) != -1)
-			if (dup2(infile_fd, STDIN_FILENO) != -1)
-				if (dup2(pipe_fd[1], STDOUT_FILENO) != -1)
-					if (close(infile_fd) != -1)
-						if (close(pipe_fd[1]) != -1)
-							return (execve(get_path(av[2], envp), ft_split(av[2], ' '), envp));
-							//return (execve("/bin/cat", ft_split(av[2], ' '), env));
-		exit_error("[>]	An error ocurred while executing child process.", 2);
-	} 				// MAIN PROCESS, READ
-	outfile_fd = open_file(av[4], OUTFILE);
-	if (waitpid(pid, NULL, 0) != -1)
-		if (close(pipe_fd[1]) != -1)
-			if (dup2(outfile_fd, STDOUT_FILENO) != -1)
-				if (dup2(pipe_fd[0], STDIN_FILENO) != -1)
-					if (close(outfile_fd) != -1)
-						if (close(pipe_fd[0]) != -1)
-							return (execve(get_path(av[3], envp), ft_split(av[3], ' '), envp));
-	exit_error("[>]	An error ocurred while executing main process.", 2);
-}*/
-
-// int	main(int ac, char **av, char **envp)
-// {
-// 	int		pipe_fd[2];
-// 	pid_t	pid1;
-// 	pid_t	pid2;
-
-// 	if (pipe(pipe_fd) == -1)
-// 		exit_error("pipe in main", 2);
-// 	if (ac == 5)
-// 	{
-// 		pid1 = fork();
-// 		if (pid1 == -1)
-// 			exit_error("(pid1) fork in main", 2);
-// 		else if (pid1 == 0)
-// 			if (infile_process(av, pipe_fd, envp) == -1)
-// 				exit_error("execve in infile_process", 2);
-// 		if (waitpid(pid1, NULL, 0) == -1)
-// 			exit_error("(pid1) waitpid in main", 2);
-// 		pid2 = fork();
-// 		if (pid2 == -1)
-// 			exit_error("(pid2) fork in main", 2);
-// 		else if (pid2 == 0)
-// 			if (outfile_process(av, pipe_fd, envp) == -1)
-// 				exit_error("execve in infile_process", 2);
-// 		close(pipe_fd[0]);
-// 		close(pipe_fd[1]);
-// 		if (waitpid(pid2, NULL, 0) == -1)
-// 			exit_error("(pid2) waitpid in main", 2);
-// 	}
-// 	else
-// 		exit_error("number of arguments", 2);
-// 	return (0);
-// }
